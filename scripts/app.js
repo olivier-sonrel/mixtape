8ab66d3e0324a3043625313d890d7852ce05e3c9
olivier-sonrel <o.sonrel@it-students.fr>
2021-06-25 15:00:48 +0200

    refactor

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,35 @@
 const appContents = document.querySelector('.app-contents');
 const startMessage = document.querySelector('.start-message');
+
+var potX;
+var potY;
+
+var socket = io(); //load socket.io-client and connect to the host that serves the page
+
 let isAppInit = false;
 appContents.style.display = 'none';
 
 window.addEventListener('keydown', init);
-window.addEventListener('click', init);
+//window.addEventListener('click', init);
+window.addEventListener("load", init);
+// window.addEventListener("load", function(){ //when page loads
+//   var lightbox = document.getElementById("light");
+//   lightbox.addEventListener("change", function() { //add event listener for when checkbox changes
+//     socket.emit("light", Number(this.checked)); //send button status to server (as 1 or 0)
+//   });
+// });
+
+// socket.on('light', function (data) { //get button status from client
+//   document.getElementById("light").checked = data; //change checkbox according to push button on Raspberry Pi
+//   socket.emit("light", data); //send push button status to back to server
+// });
+
+socket.on('Curl', function (data) { //get button status from client
+  potX = data.dataX;
+  potY = data.dataY;
+  console.log('dataX', data.dataX);
+  console.log('dataY', data.dataY);
+});
 
 function init() {
   if (isAppInit) {
